e58a1802855b43033599f625b2a5c8d9dc1ca249
Jibril <jibrilballer34@gmail.com>
2024-10-26 17:29:20 +0300

    Update structure.js

--- structure.js
+++ structure.js
@@ -1,50 +1,47 @@
-class Student {
-    constructor(studentId, name, grade = "") {
-      this.studentId = studentId;
-      this.name = name;
-      this.grade = grade;
+class Database {
+    constructor() {
+      this.university = {
+        name: "",
+        departments: [
+          {
+            name: "",
+            head: "",
+            courses: [
+              {
+                courseCode: "",
+                courseName: "",
+                credits: 0,
+                instructor: {
+                  name: "",
+                  office: ""
+                },
+                enrolledStudents: [
+                  {
+                    studentId: 0,
+                    name: "",
+                    grade: ""
+                  }
+                ]
+              }
+            ]
+          }
+        ],
+        students: [
+          {
+            studentId: 0,
+            name: "",
+            major: ""
+          }
+        ],
+        faculty: [
+          {
+            name: "",
+            department: "",
+            position: ""
+          }
+        ]
+      };
     }
   }
-  
-  class Instructor {
-    constructor(name, office) {
-      this.name = name;
-      this.office = office;
-    }
-  }
-  
-  class Course {
-    constructor(courseCode, courseName, credits, instructor, enrolledStudents = []) {
-      this.courseCode = courseCode;
-      this.courseName = courseName;
-      this.credits = credits;
-      this.instructor = instructor;
-      this.enrolledStudents = enrolledStudents;
-    }
-  }
-  
-  class Department {
-    constructor(name, head, courses = []) {
-      this.name = name;
-      this.head = head;
-      this.courses = courses;
-    }
-  }
-  
-  class FacultyMember {
-    constructor(name, department, position) {
-      this.name = name;
-      this.department = department;
-      this.position = position;
-    }
-  }
-  
-  class University {
-    constructor(name, departments = [], students = [], faculty = []) {
-      this.name = name;
-      this.departments = departments;
-      this.students = students;
-      this.faculty = faculty;
-    }
-  }
+ 
   
